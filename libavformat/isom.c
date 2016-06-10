1bc25b409fc8bb4d644f61bd6953b9ed2c47644c
Christophe Gisquet <christophe.gisquet@gmail.com>
2016-06-10 20:06:39 +0200

    isom: add another magic yuv tag

--- libavformat/isom.c
+++ libavformat/isom.c
@@ -289,6 +289,7 @@ const AVCodecTag ff_codec_movvideo_tags[] = {
     { AV_CODEC_ID_MAGICYUV, MKTAG('M', '8', 'Y', '2') },
     { AV_CODEC_ID_MAGICYUV, MKTAG('M', '8', 'Y', '4') },
     { AV_CODEC_ID_MAGICYUV, MKTAG('M', '8', 'Y', 'A') },
+    { AV_CODEC_ID_MAGICYUV, MKTAG('M', 'Y', 'U', 'V') },
 
     { AV_CODEC_ID_SHEERVIDEO, MKTAG('S', 'h', 'r', '0') },
     { AV_CODEC_ID_SHEERVIDEO, MKTAG('S', 'h', 'r', '1') },
