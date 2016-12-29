c213b5021a6358915c188493a90cb91465baa4cc
Christophe Gisquet <christophe.gisquet@gmail.com>
2016-12-29 19:53:38 +0000

    huffjoint: 4-element-wide joint tables
    The implementation reuses the 2-wide joint tables. A later test on
    reducing the number of elements tested for inclusion in these tables
    showed an important slowdown, so they are likely still useful in
    addition to the new 4-element-wide ones.
    
    No implementation for >8-bits. Validated on magicyuv and huffyuvdec.
    
    Some smarter granularity (3-wide?) could achieve better results but at
    the cost of a likely even more complex implementation.

--- libavcodec/huffjoint.h
+++ libavcodec/huffjoint.h
@@ -56,24 +56,64 @@
  * by using joint VLC dtable, otherwise read them using table1 and
  * table2 respectively.
  */
+#define GET_VLC_DUAL_INTERNAL(dst0, dst1, name, gb, dtable,         \
+                              table1, table2, bits, max_depth, OP)  \
+    if (n<=0) {                                                     \
+        int nb_bits;                                                \
+        VLC_INTERN(dst0, table1, gb, name, bits, max_depth);        \
+                                                                    \
+        UPDATE_CACHE(re, gb);                                       \
+        index = SHOW_UBITS(name, gb, bits);                         \
+        VLC_INTERN(dst1, table2, gb, name, bits, max_depth);        \
+    } else {                                                        \
+        code = dtable[index][0];                                    \
+        OP(dst0, dst1, code);                                       \
+        LAST_SKIP_BITS(name, gb, n);                                \
+    }
+
+/**
+ * Try to read into dst0 and dst1, using operation OP, 2 symbols
+ * by using joint VLC dtable, otherwise read them using table1 and
+ * table2 respectively.
+ */
 #define GET_VLC_DUAL(dst0, dst1, name, gb, dtable, table1, table2,  \
                      bits, max_depth, OP)                           \
     do {                                                            \
         unsigned int index = SHOW_UBITS(name, gb, bits);            \
         int          code, n = dtable[index][1];                    \
                                                                     \
-        if (n<=0) {                                                 \
-            int nb_bits;                                            \
-            VLC_INTERN(dst0, table1, gb, name, bits, max_depth);    \
-                                                                    \
-            UPDATE_CACHE(re, gb);                                   \
-            index = SHOW_UBITS(name, gb, bits);                     \
-            VLC_INTERN(dst1, table2, gb, name, bits, max_depth);    \
-        } else {                                                    \
-            code = dtable[index][0];                                \
-            OP(dst0, dst1, code);                                   \
-            LAST_SKIP_BITS(name, gb, n);                            \
-        }                                                           \
+        GET_VLC_DUAL_INTERNAL(dst0, dst1, name, gb, dtable,         \
+                              table1, table2, bits, max_depth, OP)  \
+    } while (0)
+
+/**
+ * Read 4 elements using multiple joint tables
+ * @param dst     Pointer of destination
+ * @param off     Offset into pointer
+ * @param Ftable  4-element-wide table
+ * @param Dtable  2-element-wide table
+ */
+#define GET_VLC_MULTI(dst, off, name, gb, Ftable, Dtable, table, bits, max_depth) \
+    do {                                                                 \
+        unsigned int index = SHOW_UBITS(name, gb, bits);                 \
+        int          code, n = Ftable[index][1];                         \
+                                                                         \
+        if (n<=0) {                                                      \
+            n = Dtable[index][1];                                        \
+            GET_VLC_DUAL_INTERNAL(dst[off+0], dst[off+1], name, gb, Dtable,\
+                                  table, table, bits, max_depth, OP8bits)\
+            /* And now for the last 2 */                                 \
+            UPDATE_CACHE(name, gb);                                      \
+            index = SHOW_UBITS(name, gb, bits);                          \
+            n = Dtable[index][1];                                        \
+            GET_VLC_DUAL_INTERNAL(dst[off+2], dst[off+3], name, gb, Dtable,\
+                                  table, table, bits, max_depth, OP8bits)\
+        } else {                                                         \
+            code = Ftable[index][0];                                     \
+            dst[off+0] =  code>>12;    dst[off+1] = (code>>8)&7;         \
+            dst[off+2] = (code>> 4)&7; dst[off+3] = code&7;              \
+            LAST_SKIP_BITS(name, gb, n);                                 \
+        }                                                                \
     } while (0)
 
 /** Default operation for reading 8 bits elements */
@@ -106,5 +146,15 @@ int ff_huff_joint_gen(VLC *vlc, void *array, int num, int numbits,
                       const uint32_t* bits0, const uint32_t* bits1,
                       const uint8_t* len0, const uint8_t* len1,
                       const uint16_t* lut0, const uint16_t* lut1);
+
+/**
+ * Generate a table suitable for decoding 4 VLCs at a time.
+ * Simplified version of ff_huff_joint_gen: single dictionary,
+ * decoded value limited to [0,15] so 4 of them fit on the 16bits of
+ * the output.
+ */
+int ff_huff_joint4_gen(VLC *vlc, void *array, int num, int numbits,
+                       const uint32_t* bits, const uint8_t* len,
+                       const uint16_t* lut);
 
 #endif /* AVCODEC_HUFF_JOINT_H */
