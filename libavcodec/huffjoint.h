c76f95bd3e615308b76c98b937fefbf8dd2af711
Christophe Gisquet <christophe.gisquet@gmail.com>
2016-12-31 12:29:56 +0000

    huffjoint: allow values of up to 16 in 4-joint

--- libavcodec/huffjoint.h
+++ libavcodec/huffjoint.h
@@ -109,9 +109,9 @@
             GET_VLC_DUAL_INTERNAL(dst[off+2], dst[off+3], name, gb, Dtable,\
                                   table, table, bits, max_depth, OP8bits)\
         } else {                                                         \
-            code = Ftable[index][0];                                     \
-            dst[off+0] =  code>>12;    dst[off+1] = (code>>8)&7;         \
-            dst[off+2] = (code>> 4)&7; dst[off+3] = code&7;              \
+            code = (unsigned int)Ftable[index][0];                       \
+            dst[off+0] =  code>>12;     dst[off+1] = (code>>8)&15;       \
+            dst[off+2] = (code>> 4)&15; dst[off+3] = code&15;            \
             LAST_SKIP_BITS(name, gb, n);                                 \
         }                                                                \
     } while (0)
